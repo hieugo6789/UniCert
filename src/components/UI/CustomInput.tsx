cbedab715e75bda00e04a98d4da638f378983e50
TinNTGaming <84866171+TinNTGaming@users.noreply.github.com>
2024-09-27 01:50:45 +0700

    login/register

---
+++ src/components/UI/CustomInput.tsx
@@ -0,0 +1,26 @@
+interface CustomInputProps {
+    type: string;
+    onChange?: () => void;
+    required?: boolean;
+    width?: string;
+    height?: string;
+    placeholder: string;
+  }
+
+const CustomInput = (props: CustomInputProps) => {
+  return (
+    <div className="mb-4">
+            <input
+              type={props.type}
+              placeholder={props.placeholder}
+              className={`${props.width} ${
+        props.height
+      } w-full px-4 py-2 border border-purple-300 rounded-lg placeholder:text-purple-400 focus:outline-none focus:ring-2 focus:ring-purple-400`}
+                onChange={props.onChange}
+                required={props.required}
+            />
+    </div>
+  )
+}
+
+export default CustomInput
--- src/page/Login&Register/Register.tsx
+++ src/page/Login&Register/Register.tsx
@@ -1,4 +1,43 @@
+import CustomButton from "../../components/UI/CustomButton";
+import CustomInput from "../../components/UI/CustomInput";
+import image from "../../assets/images/login.png";
+import { Link } from 'react-router-dom';
 const Register = () => {
-  return <div>Register</div>;
+  const handleRegister = () => {
+    console.log("register");
+  };
+  return (
+    <div className="flex justify-center items-center h-screen bg-white">
+ 
+    <div className="flex-1 flex justify-center items-center">
+      <img
+        src={image}
+        alt="Image"
+        className="w-1/2"
+      />
+    </div>
+    <div className="flex-1">
+      <div className="max-w-sm mx-auto  p-8 ">
+        <h2 className="text-2xl font-bold mb-6 text-center">Register</h2>
+        <form>
+          <CustomInput type="text" placeholder="Username" width="w-full" height="px-4 py-2"/>
+          <CustomInput type="email" placeholder="Email" width="w-full" height="px-4 py-2"/>
+          <CustomInput type="password" placeholder="Password" width="w-full" height="px-4 py-2"/>
+
+          
+          
+          <CustomButton label="Register" onClick={handleRegister} width="w-full" height="py-2" />
+        </form>
+
+        <p className="text-center text-sm text-gray-500 mt-4 bg-gray-100 w-full p-2.5 rounded-xl">
+          Already have account?{" "}
+          <Link to="/login" className="text-purple-600 font-bold hover:underline">
+            Log in
+          </Link>
+        </p>
+      </div>
+    </div>
+  </div>
+  );
 };
 export default Register;
