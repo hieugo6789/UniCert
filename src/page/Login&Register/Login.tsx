82646da600945b5b53a5022298a455bf13402340
TinNTGaming <84866171+TinNTGaming@users.noreply.github.com>
2024-09-27 15:34:50 +0700

    login/regis

--- src/page/Login&Register/Login.tsx
+++ src/page/Login&Register/Login.tsx
@@ -13,12 +13,12 @@ const Login = () => {
       <img
         src={image}
         alt="Image"
-        className="w-1/2"
+        className="w-2/3"
       />
     </div>
     <div className="flex-1">
-      <div className="max-w-sm mx-auto  p-8 ">
-        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+      <div className="max-w-md mx-auto  p-8 ">
+        <h2 className="text-3xl font-bold mb-6 text-center">Login</h2>
         <form>
           <CustomInput type="text" placeholder="Username" width="w-full" height="px-4 py-2"/>
 
