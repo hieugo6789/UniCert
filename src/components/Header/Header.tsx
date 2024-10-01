ed7ad31e07a4d08c9faf94ca1cb0a75618da3c37
Nguyễn Trung Tín <84866171+TinNTGaming@users.noreply.github.com>
2024-10-01 23:03:18 +0700

    Update Header.tsx: edit search's width

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,11 +13,11 @@ const Header = () => {
       </div>
 
       {/* Search Bar */}
-      <div className="relative">
+      <div className="relative w-4/12">
         <input
           type="text"
           placeholder="Search..."
-          className="bg-white-100 text-white rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-500"
+          className="bg-white-100 text-white w-full rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-500"
         />
         <span className="absolute right-3 top-2 text-black">
           <svg
