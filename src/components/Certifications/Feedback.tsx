d21195f2885fe37e1120b37b32b5336f445bc4f1
Nguyễn Trung Tín <84866171+TinNTGaming@users.noreply.github.com>
2024-10-11 15:33:29 +0700

    Update Feedback.tsx

--- src/components/Certifications/Feedback.tsx
+++ src/components/Certifications/Feedback.tsx
@@ -1,7 +1,25 @@
 
 const Feedback = () => {
   return (
-    <div>Feedback</div>
+    <div>
+      <h1 className="text-2xl font-bold ">Feedback</h1>
+      <div className="mt-4 w-1/2 m-auto shadow p-5 rounded-xl ">
+        <div className="flex items-center space-x-4">
+          <img src="https://randomuser.me/api/portraits" alt="" className="w-16 h-16 rounded-full" />
+          <div>
+            <h2 className="text-lg font-semibold">John Doe</h2>
+            <p className="text-gray-600">Software Engineer</p>
+          </div>
+        </div>
+        
+        <hr className="my-4" />
+
+        <p className="mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat,
+          vestibulum mi nec, ultricies metus. Donec quis luctus nunc. Morbi rhoncus, ex quis efficitur
+          suscipit, dui quam gravida nib
+        </p>
+      </div>
+    </div>
   )
 }
 
