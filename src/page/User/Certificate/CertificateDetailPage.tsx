87672eab8820548a5d55bc1cb9783be75ecdf494
Nguyễn Trung Tín <84866171+TinNTGaming@users.noreply.github.com>
2024-10-15 02:04:22 +0700

    update for deploy

--- src/page/User/Certificate/CertificateDetailPage.tsx
+++ src/page/User/Certificate/CertificateDetailPage.tsx
@@ -7,38 +7,37 @@ import { useParams } from "react-router-dom";
 import { allCertificationData } from "../../../models/certificate";
 import agent from "../../../utils/agent";
 import useCertDetail from "../../../hooks/useCertDetail";
-// import { DetailParam } from "../../../models/tableParam";
-// import { useAppDispatch } from "../../../redux/hook";
-const CertificateDetailPage = () => {
 
+const CertificateDetailPage = () => {
   const [activeTab, setActiveTab] = useState("Description");
-  const [cert, setCertificate] = useState<allCertificationData>();
+  const [cert, setCertificate] = useState<allCertificationData | undefined>(undefined);
   const id = useParams().id;
   const { state, getCertDetails } = useCertDetail();
+
   useEffect(() => {
-    const response = agent.Certificate.getCertificateDetail(
-      id?.toString() || ""
-    );
-    response.then((data) => {
-      console.log(data);
-      setCertificate(data.data);
-    });
+    const fetchData = async () => {
+      try {
+        const response = await agent.Certificate.getCertificateDetail(id?.toString() || "");
+        setCertificate(response.data);
+      } catch (error) {
+        console.error("Error fetching certificate details:", error);
+      }
+    };
+
+    fetchData();
   }, [id]);
 
   useEffect(() => {
     getCertDetails(id);
-  }, []);
+  }, [id, getCertDetails]);
+
   return (
     <div className="w-full p-4">
       <div className="flex justify-between items-center bg-purple-600 p-8 rounded-lg shadow-lg">
         {/* Left Section */}
         <div className="text-left text-white">
           <img
-            src={
-              state?.currentCert.certImage
-                ? state.currentCert.certImage
-                : certificationDefault
-            }
+            src={state?.currentCert.certImage || certificationDefault}
             alt="Logo"
             className="w-1/2"
           />
@@ -48,77 +47,38 @@ const CertificateDetailPage = () => {
           </p>
 
           <div className="mt-4 flex space-x-4">
-            {/* <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"> */}
-            <button className="inline-flex justify-center whitespace-nowrap
-             rounded-lg px-3.5 py-2.5 text-sm font-medium text-slate-200 dark:text-slate-800 
-             bg-gradient-to-r from-slate-800 to-slate-700 dark:from-slate-200 dark:to-slate-100 
-             dark:hover:bg-slate-200 shadow focus:outline-none focus:ring focus:ring-slate-500/50 
-             focus-visible:outline-none focus-visible:ring focus-visible:ring-slate-500/50 relative
-              before:absolute before:inset-0 before:rounded-[inherit] 
-              before:bg-[linear-gradient(45deg,transparent_25%,theme(colors.white/.2)_50%,transparent_75%,transparent_100%)] 
-              dark:before:bg-[linear-gradient(45deg,transparent_25%,theme(colors.white)_50%,transparent_75%,transparent_100%)] 
-              before:bg-[length:250%_250%,100%_100%] before:bg-[position:200%_0,0_0] before:bg-no-repeat before:[transition:background-position_0s_ease]
-               hover:before:bg-[position:-100%_0,0_0] hover:before:duration-[1500ms]">
+            <button className="inline-flex justify-center whitespace-nowrap rounded-lg px-3.5 py-2.5 text-sm font-medium text-slate-200 dark:text-slate-800 bg-gradient-to-r from-slate-800 to-slate-700 dark:from-slate-200 dark:to-slate-100 dark:hover:bg-slate-200 shadow focus:outline-none focus:ring focus:ring-slate-500/50 focus-visible:outline-none focus-visible:ring focus-visible:ring-slate-500/50 relative before:absolute before:inset-0 before:rounded-[inherit] before:bg-[linear-gradient(45deg,transparent_25%,theme(colors.white/.2)_50%,transparent_75%,transparent_100%)] dark:before:bg-[linear-gradient(45deg,transparent_25%,theme(colors.white)_50%,transparent_75%,transparent_100%)] before:bg-[length:250%_250%,100%_100%] before:bg-[position:200%_0,0_0] before:bg-no-repeat before:[transition:background-position_0s_ease] hover:before:bg-[position:-100%_0,0_0] hover:before:duration-[1500ms]">
               Take Exam
             </button>
-            {/* <button className="bg-white text-blue-500 py-2 px-4 rounded-md flex items-center space-x-2 hover:bg-gray-200">
-              <svg
-                className="w-5 h-5"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 3h2l1 10h13l1-10h2M16 13v6a1 1 0 001 1h1a1 1 0 001-1v-6M7 13v6a1 1 0 001 1h1a1 1 0 001-1v-6"
-                />
-              </svg>
-              <span>Add to cart</span>
-            </button> */}
           </div>
         </div>
 
         {/* Right Section */}
         <div className="bg-white p-6 rounded-lg shadow-md text-black max-w-xs">
           <h2 className="text-xl font-bold">{cert?.certName}</h2>
-          
-          {/* <p className="mt-2">{cert?.certCode}</p> */}
           <hr className="my-2" />
           <p>
             <strong>Foundation level: </strong>
             {cert?.certPrerequisite && cert.certPrerequisite.length > 0
-              ? cert.certPrerequisite
+              ? cert.certPrerequisite.join(", ")
               : "Beginner"}
           </p>
-          {/* <p>
-            <strong>Globally recognized certification</strong>
-          </p> */}
           <p className="mt-2">
             <strong>Certificate Validity: </strong>
-            {cert && cert.certValidity
-              ? cert.certValidity
-              : "This is a permanent certificate"}
+            {cert?.certValidity || "This is a permanent certificate"}
           </p>
-          <a
-            href="#"
-            className="text-blue-500 mt-4 block hover:underline"
-          >
+          <a href="#" className="text-blue-500 mt-4 block hover:underline">
             Learn More
           </a>
         </div>
       </div>
+
       <div className="flex border-b-2 border-gray-200">
         {["Description", "Exam Details", "Feedback"].map((tab) => (
           <button
             key={tab}
             onClick={() => setActiveTab(tab)}
-            className={`p-2 transition-colors duration-300 ${activeTab === tab
-              ? "border-b-4 border-blue-500 text-blue-500"
-              : ""
-              }`}
+            className={`p-2 transition-colors duration-300 ${activeTab === tab ? "border-b-4 border-blue-500 text-blue-500" : ""}`}
           >
             {tab}
           </button>
@@ -126,11 +86,12 @@ const CertificateDetailPage = () => {
       </div>
 
       <div className="p-4">
-        {activeTab === "Description" && <Description {...cert} />}
+        {activeTab === "Description" && cert && <Description {...cert} />}
         {activeTab === "Exam Details" && <ExamDetails />}
         {activeTab === "Feedback" && <Feedback />}
       </div>
     </div>
   );
 };
+
 export default CertificateDetailPage;
