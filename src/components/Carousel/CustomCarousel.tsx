f43ca257d96aef653530cd7257ef3e06c6d8b1e7
Nguyễn Trung Tín <84866171+TinNTGaming@users.noreply.github.com>
2024-10-18 02:55:39 +0700

    Update CustomCarousel.tsx

--- src/components/Carousel/CustomCarousel.tsx
+++ src/components/Carousel/CustomCarousel.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { CaretRightOutlined, CaretLeftOutlined } from "@ant-design/icons";
 import Banner from "./banner";
-import { Link } from "react-router-dom";
 
 interface Slide {
   title?: string;
