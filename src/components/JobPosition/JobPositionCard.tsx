d9f48823104c293e2d4c3d42c3a9db2e201cda25
Nguyễn Trung Tín <84866171+TinNTGaming@users.noreply.github.com>
2024-10-18 18:21:11 +0700

    Update JobPositionCard.tsx

--- src/components/JobPosition/JobPositionCard.tsx
+++ src/components/JobPosition/JobPositionCard.tsx
@@ -9,22 +9,41 @@ import { allJobPaginationData } from "../../models/jobPosition";
 const JobPositionCard = ({ job }: { job: allJobPaginationData }) => {
     const navigate = useNavigate();
     return (
-        <div className="bg-violet-100 shadow-lg rounded-lg text-white p-4 text-left hover:bg-gray-200 cursor-pointer duration-300 ease-in-out hover:scale-105"
-        onClick={()=>navigate("/job/"+job.jobPositionId)}
+        <div
+            className="bg-violet-100 shadow-lg rounded-lg text-white p-4 text-left hover:bg-gray-200 cursor-pointer duration-300 ease-in-out hover:scale-105"
+            onClick={() => navigate("/job/" + job.jobPositionId)}
         >
             <div className="mt-4">
-                <h2 className="text-xl font-bold text-gray-800 ">{job.jobPositionName}</h2>
-                <p className="text-sm text-gray-600">{job.jobPositionDescription}</p>
-                <h1>Major:
-                    {job.majorName && job.majorName.length > 0 && (
-                        <span className="text-blue-500">
-                            {" "+job.majorName.join(", ")}
-                        </span>
+                <h2 className="text-xl font-bold text-gray-800">{job.jobPositionName}</h2>
+                {/* <p className="text-sm text-gray-600">{job.jobPositionDescription}</p> */}
+                <div
+                    className="prose list-disc whitespace-pre-wrap text-sm text-gray-600"
+                    dangerouslySetInnerHTML={{ __html: job.jobPositionDescription || "" }}
+                />
+                <h1 className="text-black">
+                    Major:
+                    {job.majorDetails.length.toString() === "0" ? (
+                        " No major."
+                    ) : (
+                        job.majorDetails
+                            .map((major, index) => (
+                                <span
+                                    key={major.majorId}
+                                    onClick={(e) => {
+                                        e.stopPropagation();
+                                        navigate("/major/" + major.majorId);
+                                    }}
+                                >
+                                    {" " + major.majorName}
+                                    {index < job.majorDetails.length - 1 ? ", " : "."}
+                                </span>
+                            ))
                     )}
-
                 </h1>
             </div>
         </div>
+
+
     );
 };
 
