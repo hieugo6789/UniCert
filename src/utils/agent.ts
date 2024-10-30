81594cf5f11e76c5634b574909b6f90253fe8122
Nguyễn Trung Tín <84866171+TinNTGaming@users.noreply.github.com>
2024-10-30 23:42:29 +0700

    Update agent.ts

--- src/utils/agent.ts
+++ src/utils/agent.ts
@@ -220,7 +220,7 @@ const CertType = {
 const Cart = {
   getCartByUserId: (userId: string) => requests.get(`api/v1/cart/${userId}`),
   updateCart: (userId: string, input: updateCart) =>
-    requests.put(`api/v1/cart${userId}`, input),
+    requests.put(`api/v1/cart/${userId}`, input),
 };
 
 const Enrollment = {
