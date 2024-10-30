80ceaba1c34f44b3f3f8449ed08bfc4c374e7592
Nguyễn Trung Tín <84866171+TinNTGaming@users.noreply.github.com>
2024-10-30 23:44:08 +0700

    Update CourseCard.tsx

--- src/components/Course/CourseCard.tsx
+++ src/components/Course/CourseCard.tsx
@@ -1,23 +1,29 @@
 import { useNavigate } from "react-router-dom";
 import { allCoursePaginationData } from "../../models/course";
 
+interface CourseCardProps {
+  course: allCoursePaginationData;
+  onClick: () => void;
+}
 
-
-const CourseCard = (course:allCoursePaginationData) => {
+const CourseCard = ({ course, onClick }: CourseCardProps) => {
   const navigate = useNavigate();
   return (
-    
-        <div className="border p-6 rounded-lg shadow-lg" onClick={()=>navigate("/course/"+course.courseId)}>
-          <h2 className="text-xl font-bold">{course.courseName}</h2>
-          <p className="text-gray-500 mt-2">{course.courseCode}</p>
-          <img src={course.courseImage} alt={course.courseName} className="mt-4 h-30 m-auto shadow-lg" />
-          {/* course fee */}
-          <p className="text-black-500 mt-4">Course Fee: {course.courseFee}</p>
-          {/* course duration */}
-          <p className="text-black-500 mt-4">Course Duration: {course.courseTime}</p>
-          <a href={"/"} className="text-blue-500 mt-4 block rounded-full text-l text-right">Add To Cart {">"}</a>
-        </div>
-      
+
+    <div className="border p-6 rounded-lg shadow-lg" onClick={() => navigate("/course/" + course.courseId)}>
+      <h2 className="text-xl font-bold">{course.courseName}</h2>
+      <p className="text-gray-500 mt-2">{course.courseCode}</p>
+      <img src={course.courseImage} alt={course.courseName} className="mt-4 h-30 m-auto shadow-lg" />
+      {/* course fee */}
+      <p className="text-black-500 mt-4">Course Fee: {course.courseFee}</p>
+      {/* course duration */}
+      <p className="text-black-500 mt-4">Course Duration: {course.courseTime}</p>
+      <p onClick={(e) => {
+          e.stopPropagation(); // Ngăn sự kiện click lan truyền lên component cha
+          onClick();
+        }}className="text-blue-500 mt-4 block rounded-full text-l text-right cursor-pointer hover:text-blue-300 hover:underline">Add To Cart {">"}</p>
+    </div>
+
   )
 }
 
