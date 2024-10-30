d250451692db327eee91ab873cfb31b7bc787f2a
Nguyễn Trung Tín <84866171+TinNTGaming@users.noreply.github.com>
2024-10-30 22:39:50 +0700

    Update HistoryPage.tsx

--- src/page/User/History/HistoryPage.tsx
+++ src/page/User/History/HistoryPage.tsx
@@ -1,161 +1,41 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import CourseCard from "../../../components/Course/CourseCard";
-import ExamSimulaCard from "../../../components/Exam/ExamSimulaCard"; 
-import { allExamPaginationData } from "../../../models/simulationExam";
-import { allCoursePaginationData } from "../../../models/course";
-
+// import { allExamPaginationData } from "../../../models/simulationExam";
+// import { allCoursePaginationData } from "../../../models/course";
+import useCourseEnrollment from "../../../hooks/Enrollment/useCourse";
+import useExamEnrollment from "../../../hooks/Enrollment/useExam";
+import Loading from "../../../components/UI/Loading";
+import HistoryCourseCard from "../../../components/Course/HistoryCourseCard";
+import HistoryExamCard from "../../../components/Exam/HistoryExamCard";
+import { courseEnrollment, examEnrollment } from "../../../models/enrollment";
+import Cookies from "js-cookie";
 const HistoryPage = () => {
-  const { id } = useParams<{ id: string }>(); // Extract userID from the URL
+  // const { id } = useParams<{ id: string }>(); // Extract userID from the URL
+  const id = Cookies.get("userId");
   const [activeTab, setActiveTab] = useState("exams"); // Current active tab
-  const [purchasedExams, setPurchasedExams] = useState<allExamPaginationData[]>([]);
-  const [purchasedCourses, setPurchasedCourses] = useState<allCoursePaginationData[]>([]);
-
+  const [purchasedExams, setPurchasedExams] = useState<examEnrollment[]>([]);
+  const [purchasedCourses, setPurchasedCourses] = useState<courseEnrollment[]>([]);
+  const { courseEnrollment, loading: courseLoad, refetchCourseEnrollments } = useCourseEnrollment({ userId: id?.toString() || "" });
+  const { examEnrollment, loading: examLoad, refetchExamEnrollments } = useExamEnrollment({ userId: id?.toString() || "" });
   useEffect(() => {
     // Giả lập dữ liệu các kỳ thi đã mua
     const fetchPurchasedExams = () => {
-        const exams = [
-            {
-              examId: 14,
-              examName: "AWS2 Certified Machine Learning - Specialty Simualtion Exam",
-              examCode: "AWS-CML-SE-2",
-              certId: 62,
-              examDescription: "This is the Simulation Exam for AWS Certified",
-              examFee: 100000,
-              examDiscountFee: 60000,
-              examImage: "https://marketplace.canva.com/EAFH_f4DrV0/2/0/1600w/canva-gold-luxury-certificate-of-completion-template-yz6w30Bp85w.jpg", // Thay bằng đường dẫn thực tế
-            },
-            {
-              examId: 15,
-              examName: "AWS2 Certified Machine Learning - Specialty Simualtion Exam",
-              examCode: "AWS-CML-SE-2",
-              certId: 62,
-              examDescription: "This is the Simulation Exam for AWS Certified",
-              examFee: 100000,
-              examDiscountFee: 6000,
-              examImage: "https://marketplace.canva.com/EAFH_f4DrV0/2/0/1600w/canva-gold-luxury-certificate-of-completion-template-yz6w30Bp85w.jpg", // Thay bằng đường dẫn thực tế
-            },
-            {
-              examId: 16,
-              examName: "AWS3 Certified Machine Learning - Specialty Simualtion Exam",
-              examCode: "AWS-CML-SE-3",
-              certId: 62,
-              examDescription: "Assess your skills in Node.js.",
-              examFee: 100000,
-              examDiscountFee: 100000,
-              examImage: "https://marketplace.canva.com/EAFH_f4DrV0/2/0/1600w/canva-gold-luxury-certificate-of-completion-template-yz6w30Bp85w.jpg", // Thay bằng đường dẫn thực tế
-            },
-          ];
-      setPurchasedExams(exams);
+      refetchExamEnrollments(id?.toString() || "");
     };
 
     // Giả lập dữ liệu các khóa học đã mua
     const fetchPurchasedCourses = () => {
-        const courses: allCoursePaginationData[] = [
-            {
-              courseId: "20",
-              courseName: "ISTQB",
-              courseCode: "ISTQB001",
-              courseTime: "40 hours",
-              courseDescription: "International Software Testing Qualification Board course.",
-              courseFee: 100,
-              courseDiscountFee: 90,
-              courseImage: "https://marketplace.canva.com/EAFH_f4DrV0/2/0/1600w/canva-gold-luxury-certificate-of-completion-template-yz6w30Bp85w.jpg",
-              certId: "49",
-              certificationDetails: [
-                {
-                  certId: 49,
-                  certName: "AWS Certified Cloud Practitioner",
-                  certCode: "AWSCCP",
-                  certDescription: "The AWS Certified Cloud Practitioner validates foundational understanding of AWS Cloud.",
-                  certImage: "https://d1.awsstatic.com/certification/badges/AWS-Certified-Cloud-Practitioner_badge_150x150.17da917fbddc5383838d9f8209d2030c8d99f31e.png",
-                  typeName: "Foundation",
-                }
-              ],
-              voucherDetails: [
-                {
-                  voucherId: 1,
-                  voucherName: "Voucher discount 10%",
-                  voucherDescription: "10% off on ISTQB course.",
-                  percentage: 10,
-                  creationDate: new Date("2024-10-01"),
-                  expiryDate: new Date("2024-12-31"),
-                  voucherStatus: true,
-                }
-              ],
-            },
-            {
-              courseId: "24",
-              courseName: "IBM Front-End Developer Professional Course",
-              courseCode: "IBMCourse",
-              courseTime: "100 hours",
-              courseDescription: "A comprehensive course for front-end development using HTML, CSS, JavaScript, and React.",
-              courseFee: 200,
-              courseDiscountFee: 180,
-              courseImage: "https://i.ytimg.com/vi/fcxPoGeMpoQ/maxresdefault.jpg",
-              certId: "55",
-              certificationDetails: [
-                {
-                  certId: 55,
-                  certName: "IBM Front-End Developer Professional Certificate",
-                  certCode: "IBMFEDP",
-                  certDescription: "This certificate focuses on front-end development skills.",
-                  certImage: "https://images.credly.com/images/e646f624-ca3d-4917-9e90-16a051497bdb/image.png",
-                  typeName: "Professional",
-                }
-              ],
-              voucherDetails: [
-                {
-                  voucherId: 2,
-                  voucherName: "Voucher discount 5%",
-                  voucherDescription: "5% off on IBM Front-End Developer course.",
-                  percentage: 5,
-                  creationDate: new Date("2024-10-10"),
-                  expiryDate: new Date("2024-12-31"),
-                  voucherStatus: true,
-                }
-              ],
-            },
-            {
-              courseId: "26",
-              courseName: "IBM Back-End Development Course",
-              courseCode: "IBMBE",
-              courseTime: "60 hours",
-              courseDescription: "A beginner-friendly course for back-end development covering databases, APIs, and server-side programming.",
-              courseFee: 250,
-              courseDiscountFee: 225,
-              courseImage: "https://i.ytimg.com/vi/HGuAXNrBJP8/maxresdefault.jpg",
-              certId: "56",
-              certificationDetails: [
-                {
-                  certId: 56,
-                  certName: "IBM Back-End Development Professional Certificate",
-                  certCode: "IBMBEDP",
-                  certDescription: "This certificate focuses on back-end development skills.",
-                  certImage: "https://images.credly.com/images/5aa05f53-1a60-4913-bf7e-e356f34bdb7e/image.png",
-                  typeName: "Professional",
-                }
-              ],
-              voucherDetails: [
-                {
-                  voucherId: 3,
-                  voucherName: "Voucher discount 15%",
-                  voucherDescription: "15% off on IBM Back-End Development course.",
-                  percentage: 15,
-                  creationDate: new Date("2024-10-15"),
-                  expiryDate: new Date("2024-12-31"),
-                  voucherStatus: true,
-                }
-              ],
-            }
-          ];
-      setPurchasedCourses(courses);
+      refetchCourseEnrollments(id?.toString() || "");
     };
 
     fetchPurchasedExams();
     fetchPurchasedCourses();
   }, [id]);
-
+  useEffect(() => {
+    setPurchasedExams(examEnrollment);
+  }, [examEnrollment]);
+  useEffect(() => {
+    setPurchasedCourses(courseEnrollment);
+  }, [courseEnrollment]);
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4">History</h1>
@@ -181,7 +61,7 @@ const HistoryPage = () => {
             <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-10 py-10">
               {purchasedExams.length > 0 ? (
                 purchasedExams.map((exam) => (
-                  <ExamSimulaCard key={exam.examId} {...exam} />
+                  <HistoryExamCard key={exam.examEnrollmentId} enrollment={exam} />
                 ))
               ) : (
                 <p>No purchased exams found.</p>
@@ -194,7 +74,7 @@ const HistoryPage = () => {
             <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-10 py-10">
               {purchasedCourses.length > 0 ? (
                 purchasedCourses.map((course) => (
-                  <CourseCard key={course.courseId} {...course} />
+                  <HistoryCourseCard key={course.courseEnrollmentId} enrollment={course} />
                 ))
               ) : (
                 <p>No purchased courses found.</p>
@@ -203,6 +83,8 @@ const HistoryPage = () => {
           </div>
         )}
       </div>
+
+      {examLoad && courseLoad && <Loading />}
     </div>
   );
 };
