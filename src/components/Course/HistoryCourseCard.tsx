74c0daf6dbc62c1fa158b7ae15545da88e091d72
Nguyễn Trung Tín <84866171+TinNTGaming@users.noreply.github.com>
2024-11-01 12:52:36 +0700

    Update HistoryCourseCard.tsx

--- src/components/Course/HistoryCourseCard.tsx
+++ src/components/Course/HistoryCourseCard.tsx
@@ -1,53 +1,77 @@
-
+import React, { useState } from 'react';
 import { courseEnrollment } from '../../models/enrollment';
+import CustomButton from '../UI/CustomButton';
+import { useCreatePayment } from '../../hooks/Payment/useCreatePayment';
+import Cookies from 'js-cookie';
+import coin from "../../assets/images/coin.png";
+import { showToast } from '../../utils/toastUtils';
 interface CourseEnrollmentCardProps {
-    enrollment: courseEnrollment;
-  }
+  enrollment: courseEnrollment;
+}
+
 const HistoryCourseCard: React.FC<CourseEnrollmentCardProps> = ({ enrollment }) => {
-    return (
-      <div className="max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
-        <div className="p-4">
-          <h2 className="text-2xl font-bold text-gray-800 mb-2">
-            Enrollment ID: {enrollment.courseEnrollmentId}
-          </h2>
-          <p className="text-gray-600">
-            Date: {new Date(enrollment.courseEnrollmentDate).toLocaleDateString()}
-          </p>
-          <p className={`text-sm font-semibold mt-1 ${enrollment.courseEnrollmentStatus === 'completed' ? 'text-green-500' : 'text-yellow-500'}`}>
-            Status: {enrollment.courseEnrollmentStatus}
-          </p>
-          <p className="text-lg font-bold mt-2 text-gray-800">Total Price: ${enrollment.totalPrice}</p>
-  
-          <h3 className="text-lg font-semibold mt-4 text-gray-700">Courses:</h3>
-          <div className="mt-2 space-y-4">
-            {enrollment.courseDetails.map((course) => (
-              <div key={course.courseId} className="flex items-center">
-                <img
-                  src={course.courseImage}
-                  alt={course.courseName}
-                  className="w-16 h-16 rounded-full mr-4"
-                />
-                <div>
-                  <p className="text-lg font-medium text-gray-800">{course.courseName}</p>
-                  <p className="text-gray-500">Code: {course.courseCode}</p>
-                  <p className="text-gray-500">
-                    
-                    {course.courseDiscountFee > 0 ? (
-                      <div>
-                        Fee: ${course.courseDiscountFee}{' '}
-                        <span className="text-red-500 line-through">${course.courseFee}</span>
-                      </div>
-                    ) : (
-                      <span>Fee: ${course.courseFee}</span>
-                    )}
-                  </p>
-                </div>
-              </div>
-            ))}
-          </div>
+  const {handleCreatePayment} = useCreatePayment();
+  const userId = Cookies.get("userId");
+  const handlePayment  =  async() => {
+    await handleCreatePayment({
+      userId: userId?.toString() || "",
+      examEnrollmentId: 0,
+      courseEnrollmentId: enrollment.courseEnrollmentId,
+    });
+    showToast("Payment completed successfully", "success");
+
+  }
+  const [enrollStatus, setEnrollStatus] = useState(enrollment.courseEnrollmentStatus);
+  return (
+    <div className="flex flex-col md:flex-row 
+    shadow-xl rounded-xl bg-gray-200 py-1">
+
+      {/* Thông tin Enrollment */}
+      <div className="p-4 flex-1">
+        <h2 className="text-xl font-bold text-gray-800">
+          Enrollment ID: {enrollment.courseEnrollmentId}
+        </h2>
+        <p className="text-sm text-gray-500">
+          Date: {new Date(enrollment.courseEnrollmentDate).toLocaleDateString()}
+        </p>
+        <p
+          className={`text-sm font-semibold mt-1 ${enrollStatus === 'Completed'
+            ? 'text-green-500'
+            : 'text-yellow-500'
+            }`}
+        >
+          Status: {enrollStatus}
+        </p>
+        <p className="text-lg font-bold text-gray-800 mt-2 flex items-center gap-1">
+          Total Price: {enrollment.totalPrice} 
+          <img src={coin} alt="coin" className='h-5'/>
+        </p>
+        {enrollStatus === 'OnGoing' && (
+          <CustomButton
+            className="mt-4 bg-purple-600 hover:bg-purple-700 text-white py-2 px-4 rounded-md"
+            onClick={() => {handlePayment();setEnrollStatus('Completed')}}
+            label="Complete Payment"
+          />
+        )}
+      </div>
+
+      {/* Danh sách Courses/Exams */}
+      <div className='grid mr-3'>
+        <p>Course List</p>
+        <div className="p-4 flex flex-wrap gap-1">
+          {enrollment.courseDetails.map((course) => (
+            <div key={course.courseId} className="flex flex-col items-center">
+              <img
+                src={course.courseImage}
+                alt={course.courseName}
+                className="w-20 h-20 object-cover rounded-md object-center"
+              />
+            </div>
+          ))}
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default HistoryCourseCard;
