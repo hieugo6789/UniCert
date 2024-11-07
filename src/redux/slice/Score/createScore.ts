a8f4bf18ea18bacad43174744fadc762cb30a5a5
Nguyễn Trung Tín <84866171+TinNTGaming@users.noreply.github.com>
2024-11-07 18:15:31 +0700

    Add files via upload

---
+++ src/redux/slice/Score/createScore.ts
@@ -0,0 +1,40 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { createScore } from "../../../models/score";
+
+export interface CrateScore {
+  isCreating: boolean;
+  createdScore: createScore | null;
+  error: boolean;
+}
+
+const initialState: CrateScore = {
+  isCreating: false,
+  createdScore: null,
+  error: false,
+};
+
+const createScoreSlice = createSlice({
+  name: "createScorePosition",
+  initialState,
+  reducers: {
+    createScoreStart: (state) => {
+      state.isCreating = true;
+      state.createdScore = null;
+      state.error = false;
+    },
+    createScoreSuccess: (state, action: PayloadAction<createScore>) => {
+      state.isCreating = false;
+      state.createdScore = action.payload;
+      state.error = false;
+    },
+    createScoreFailure: (state) => {
+      state.isCreating = false;
+      state.createdScore = null;
+      state.error = true;
+    },
+  },
+});
+
+export const { createScoreStart, createScoreSuccess, createScoreFailure } =
+  createScoreSlice.actions;
+export default createScoreSlice.reducer;
