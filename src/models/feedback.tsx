5e58608380723b4ca45d756c63677473673a148c
hieugo6789 <hieugo240103@gmail.com>
2024-11-11 16:02:42 +0700

    fix model feedback

--- src/models/feedback.tsx
+++ src/models/feedback.tsx
@@ -5,6 +5,7 @@ export interface feedbackPagination {
   examId: number;
   feedbackCreatedAt: Date;
   feedbackImage?: string;
+  examPermission: string;
   userDetails: {
     userId: number;
     username: string;
@@ -18,6 +19,7 @@ export interface currentFeedback {
   examId: number;
   feedbackCreatedAt: Date;
   feedbackImage: string;
+  examPermission: string;
   userDetails: {
     userId: number;
     username: string;
