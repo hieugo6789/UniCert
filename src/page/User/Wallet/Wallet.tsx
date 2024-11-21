7a826cedade53e038c1f8c3523fa1009850d0f31
Nguyễn Trung Tín <84866171+TinNTGaming@users.noreply.github.com>
2024-11-22 04:39:05 +0700

    Update Wallet.tsx

--- src/page/User/Wallet/Wallet.tsx
+++ src/page/User/Wallet/Wallet.tsx
@@ -47,15 +47,15 @@ const Wallet = () => {
   return (
     <>
       <div className="flex justify-end mr-36">
-        <div className="bg-white shadow-md rounded-xl px-4 py-2 flex items-center justify-center space-x-2 max-w-72 h-full">
-          <span className=" text-gray-700">Your Coins:</span>
+        <div className="bg-white dark:bg-gray-800 shadow-md rounded-xl px-4 py-2 flex items-center justify-center space-x-2 max-w-72 h-full">
+          <span className="text-gray-700 dark:text-gray-300">Your Coins:</span>
           <div className="flex items-center">
             <img
               src={Coin}
               alt="Coin Icon"
               className="w-5 h-5"
             />
-            <span className="ml-1 text-yellow-600 font-bold">
+            <span className="ml-1 text-yellow-600 dark:text-yellow-500 font-bold">
               {userId ? wallets[userId]?.point : 0}
             </span>
           </div>
@@ -65,7 +65,7 @@ const Wallet = () => {
 
       <div className="flex justify-center min-h-[30vh]">
         <div className="w-full max-w-3xl">
-          <h1 className="text-2xl font-bold">History payment</h1>
+          <h1 className="text-2xl font-bold dark:text-white">History payment</h1>
           <div>
             {parsedUserId && historyTransaction.length > 0 ? (
               historyTransaction
@@ -73,7 +73,7 @@ const Wallet = () => {
                 .map((transaction, index) => (
                   <div key={index}>
                     <div className="flex justify-center items-center mb-2">
-                      <p className="text-gray-400">
+                      <p className="text-gray-400 dark:text-gray-500">
                         {new Date(transaction.createdAt).toLocaleString(
                           "en-US",
                           {
@@ -87,24 +87,24 @@ const Wallet = () => {
                         )}
                       </p>
                     </div>
-                    <div className=" flex mb-4 bg-white rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out">
-                      <div className="flex flex-col items-center w-32 bg-slate-50 py-3">
+                    <div className="flex mb-4 bg-white dark:bg-gray-800 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out">
+                      <div className="flex flex-col items-center w-32 bg-slate-50 dark:bg-gray-700 py-3">
                         <img
                           src={Coin}
                           alt="Coin Icon"
                           className="w-12 h-12"
                         />
                         <div className="mt-2">
-                          <p className="text-lg font-semibold text-yellow-600">
+                          <p className="text-lg font-semibold text-yellow-600 dark:text-yellow-500">
                             +{transaction.point}
                           </p>
                         </div>
                       </div>
-                      <div className="flex flex-col justify-center p-4 text-gray-700 w-full">
-                        <p className=" mb-2">
+                      <div className="flex flex-col justify-center p-4 text-gray-700 dark:text-gray-300 w-full">
+                        <p className="mb-2">
                           {transaction.amount.toLocaleString("vi-VN")} VNĐ
                         </p>
-                        <p className="text-sm font-medium text-gray-500">
+                        <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
                           {transaction.transDesription}
                         </p>
                       </div>
@@ -112,7 +112,7 @@ const Wallet = () => {
                   </div>
                 ))
             ) : (
-              <p className="text-center text-gray-500">
+              <p className="text-center text-gray-500 dark:text-gray-400">
                 No transaction history available
               </p>
             )}
