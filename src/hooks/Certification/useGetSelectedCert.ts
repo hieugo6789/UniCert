dafd82bfc336c39ae94c9208850bda03aa7dbfd4
Nguyễn Trung Tín <84866171+TinNTGaming@users.noreply.github.com>
2024-12-04 05:39:44 +0700

    Update useGetSelectedCert.ts

--- src/hooks/Certification/useGetSelectedCert.ts
+++ src/hooks/Certification/useGetSelectedCert.ts
@@ -1,25 +1,36 @@
 import { useState } from "react";
-import { useAppDispatch } from "../../redux/hook";
 import { allCertificationData } from "../../models/certificate";
-import { fetchCertificatesByUser } from "../../redux/slice/Certification/getSelectedCertSlice";
+import agent from "../../utils/agent";
 
 const useGetSelectedCert = () => {
-  const dispatch = useAppDispatch();
   const [certificate, setCertificate] = useState<allCertificationData[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
   const fetchSelectedCert = async (userId: string) => {
     setLoading(true);
     try {
-      const response = await dispatch(fetchCertificatesByUser(userId));
-      setCertificate(response.payload.data || []);
+      // Bước 1: Lấy danh sách chứng chỉ từ userId
+      const response = await agent.selectedCert.getByUser(userId);
+      const data = response; // Ví dụ: {certId: 115, certName: 'Google Data Analytics Certification'}
+      // console.log(data);
+      // Bước 2: Gọi API để lấy chi tiết chứng chỉ dựa trên certId
+      const certs = await Promise.all(
+        data.map(async (cert: { certId: number }) => {
+          const certDetail = await agent.Certificate.getCertificateDetail(cert.certId);
+          return certDetail.data; // Ví dụ: {certId: 115, certName: 'Google Data Analytics Certification'}
+        })
+      );
+
+      // Bước 3: Lưu vào state certificate
+      setCertificate(certs);
     } catch (error) {
-      console.error("Error fetching vouchers:", error);
+      console.error("Error fetching certificates:", error);
     } finally {
       setLoading(false);
     }
   };
 
   return { certificate, loading, fetchSelectedCert };
-}
+};
+
 export default useGetSelectedCert;
