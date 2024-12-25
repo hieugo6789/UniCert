9b8bc97e787ce8abf89449208a289968954899ff
Nguyễn Trung Tín <84866171+TinNTGaming@users.noreply.github.com>
2024-12-26 00:13:50 +0700

    Update PreviewExam.tsx

--- src/page/User/Exam/PreviewExam.tsx
+++ src/page/User/Exam/PreviewExam.tsx
@@ -51,7 +51,7 @@ const PreviewExam = () => {
             </div>
         );
     }
-    
+
     return (
         <div className="p-6 bg-gray-50 min-h-screen dark:bg-gray-900">
             {/* nút back exam fixed */}
@@ -74,22 +74,14 @@ const PreviewExam = () => {
                         key={question.questionId}
                         className="mb-6 p-5 rounded-lg shadow-md bg-gray-100 dark:bg-gray-700"
                     >
-                        <div className="flex justify-between items-center mb-3">
-                            {/* <h3 className="font-bold text-lg text-gray-800 dark:text-gray-200"> */}
-                            {/* {question.questionName} */}
-                            {/* {question.questionId}
-                                {question.questionType} */}
-                            {/* </h3> */}
-                            <div
-                                className="prose prose-lg dark:prose-invert max-w-none text-gray-700 dark:text-gray-300 leading-relaxed"
-                                dangerouslySetInnerHTML={{
-                                    __html: question.questionName || "",
-                                }}
-                            />
-                            <span className="text-sm font-medium text-gray-600 dark:text-gray-300">
-                                Score: {question.scoreValue}
-                            </span>
-                        </div>
+
+
+                        <div
+                            className="prose prose-lg dark:prose-invert max-w-none text-gray-700 dark:text-gray-300 leading-relaxed"
+                            dangerouslySetInnerHTML={{
+                                __html: question.questionName || "",
+                            }}
+                        />
 
                         {/* Multiple Choice Question */}
                         {question.questionType.toLowerCase() === "choice" ? (
@@ -99,10 +91,10 @@ const PreviewExam = () => {
                                         <li
                                             key={answer.answerId}
                                             className={`p-2 rounded-md ${answer.isCorrect && question.userAnswersForChoice.includes(answer.answerId)
-                                                    ? "bg-green-100 border-l-4 border-green-500 text-green-700"
-                                                    : answer.isCorrect
-                                                        ? "bg-green-100 text-green-700"
-                                                        : "bg-gray-200 text-gray-800 dark:bg-gray-600"
+                                                ? "bg-green-100 border-l-4 border-green-500 text-green-700"
+                                                : answer.isCorrect
+                                                    ? "bg-green-100 text-green-700"
+                                                    : "bg-gray-200 text-gray-800 dark:bg-gray-600"
                                                 } 
                                             ${question.userAnswersForChoice.includes(answer.answerId) && !answer.isCorrect
                                                     ? "bg-red-300 border-l-4 border-red-500 text-red-700"
@@ -110,8 +102,8 @@ const PreviewExam = () => {
                                         >
                                             <span
                                                 className={`font-medium ${question.userAnswersForChoice.includes(answer.answerId)
-                                                        ? "text-black-600"
-                                                        : ""
+                                                    ? "text-black-600"
+                                                    : ""
                                                     }`}
                                             >
                                                 {answer.text}
@@ -142,10 +134,16 @@ const PreviewExam = () => {
                                 </p>
                             </div>
                         )}
+                        <div className="flex justify-between items-center mt-4">
+                            <span className={`text-md font-bold text-gray-600 dark:text-gray-300 mb-2 ${question.isCorrectQuestion ? "text-green-500" : "text-red-500"}`}>
+                                Score: {question.scoreValue}
+                            </span>
+                            <p className="mt-4 text-xs text-gray-500 dark:text-gray-400">
+                                Submitted At: {new Date(question.submittedAt).toLocaleString()}
+                            </p>
 
-                        <p className="mt-4 text-xs text-gray-500 dark:text-gray-400">
-                            Submitted At: {new Date(question.submittedAt).toLocaleString()}
-                        </p>
+                        </div>
+
                     </div>
                 ))}
             </div>
