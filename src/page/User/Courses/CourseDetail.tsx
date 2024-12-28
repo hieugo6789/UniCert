6456fdd3e765b28b4f6200fb4a370357190a3a38
Nguyễn Trung Tín <84866171+TinNTGaming@users.noreply.github.com>
2024-12-28 17:39:09 +0700

    Update CourseDetail.tsx

--- src/page/User/Courses/CourseDetail.tsx
+++ src/page/User/Courses/CourseDetail.tsx
@@ -286,6 +286,7 @@ const CourseDetail = () => {
             >
                 <div className="p-4">
                 <div className="space-y-4">
+                <p className="text-gray-400 dark:text-gray-200 mt-2">Note: Valid for 3 days only.</p>
                     <div className="flex justify-between items-center">
                     <span className="text-gray-600 dark:text-gray-300">Course Price:</span>
                     <span className="flex items-center gap-2 font-medium">
