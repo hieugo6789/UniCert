b68b3431794bbf94044f83fbbd8668f4a38785af
Nguyễn Trung Tín <84866171+TinNTGaming@users.noreply.github.com>
2024-12-30 21:04:07 +0700

    Update useCreateExam.ts

--- src/hooks/Enrollment/useCreateExam.ts
+++ src/hooks/Enrollment/useCreateExam.ts
@@ -11,11 +11,18 @@ export function useCreateExamEnrollment() {
   const state = useAppSelector((state) => state.createExamEnrollment);
   const dispatch = useAppDispatch();
 
-  const handleCreateExamEnrollment = async (input: createExamEnrollment) => {
+  const handleCreateExamEnrollment = async (input: createExamEnrollment, voucherIds?: number[]) => {
     dispatch(createExamEnrollmentStart());
     try {
       const response = await agent.Enrollment.createExamEnroll(input);
       dispatch(createExamEnrollmentSuccess(response));
+      if (voucherIds) {
+        console.log("Exam enrollment created successfully:", response);
+        const eErollmentId = response.data.examEnrollmentId;
+        console.log("Exam enrollment id:", eErollmentId);
+        const data = await agent.Enrollment.addVoucherToCart(eErollmentId, { userId: input.userId, simulation_Exams: input.simulation_Exams, voucherIds: voucherIds ? voucherIds : [] });
+        console.log("Voucher added to cart successfully:", data);
+      }
     } catch (error) {
       console.error("Error creating exam enrollment:", error);
       dispatch(createExamEnrollmentFailure());
