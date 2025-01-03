8366da2ff07949394917f4dd436ca5d50addc2ab
Nguyễn Trung Tín <84866171+TinNTGaming@users.noreply.github.com>
2025-01-03 23:22:31 +0700

    Update useCreatePayment.ts

--- src/hooks/Payment/useCreatePayment.ts
+++ src/hooks/Payment/useCreatePayment.ts
@@ -14,13 +14,14 @@ export function useCreatePayment() {
   const handleCreatePayment = async (input: createPayment, simulation_Exam?: number[], voucherIds?: number[]) => {
     dispatch(createPaymentStart());
     try {
+      if (voucherIds) {
+        const data = await agent.Enrollment.addVoucherToCart(input.examEnrollmentId, { userId: input.userId, simulation_Exams: simulation_Exam ? simulation_Exam : [], voucherIds: voucherIds ? voucherIds : [] });
+        console.log(data);
+      }
       const response = await agent.Payment.createPayment(input);
       console.log(response);
       dispatch(createPaymentSuccess(response));
-      if (voucherIds) {
-        const data = await agent.Enrollment.addVoucherToCart(response.data.examEnrollmentId, { userId: input.userId, simulation_Exams: simulation_Exam ? simulation_Exam : [], voucherIds: voucherIds ? voucherIds : [] });
-        console.log(data);
-      }
+      
     } catch (error) {
       console.error("Error creating payment:", error);
       dispatch(createPaymentFailure());
